35b55fdab14915be3caf6be3e42ab574674e2bce
Babak Bayati <bbkbyti@Babaks-MacBook-Pro.local>
2021-08-30 23:44:08 -0500

    styling textbook detail page on small size screens - media query

--- client/src/screens/TextbookDetail.jsx
+++ client/src/screens/TextbookDetail.jsx
@@ -28,38 +28,37 @@ const BooksDetail = (props) => {
         console.log(resp)
         history.push('/textbooks')
     }
-    console.log(textbook);
     return (
-        <div className="grid grid-cols-3 min-h-screen bg-blue-50 ">
-            <div className="mt-20">
-                <img
+        <div className=" lg:font-serif lg:grid lg:grid-cols-3 lg:min-h-screen lg:bg-blue-50 flex flex-col justify-evenly min-h-screen font-serif">
+            <div className="lg:mt-20 flex justify-center">
+                <img className='w-7/12'
                     src={textbook.img_url}
                     alt={textbook.title}
                 />
                 </div>
                 
-                    <div className="lg:flex lg:flex-col lg:justify-center lg:pr-40 text-2xl font-bold mb-32 ">
+                    <div className=" lg:flex lg:flex-col lg:justify-center lg:pr-40 lg:text-2xl lg:font-bold lg:mb-32 flex flex-col px-7">
 
-                        <div className="bg-blue-100 mb-1">Title: {textbook.title}</div>
+                        <div className="lg:bg-blue-100 lg:font-extrabold lg:mb-1 bg-gray-200 my-1 rounded-xl py-2 font-extrabold">Title: {textbook.title}</div>
                         
-                        <div className="bg-blue-100 mb-1">Author: {textbook.author}</div>
+                        <div className="lg:bg-blue-100 lg:mb-1 bg-gray-200 my-1 rounded-xl py-2 px-3 font-bold">Author(s): {textbook.author}</div>
                         
-                        <div className="bg-blue-100 mb-1">Edition: {textbook.edition}</div>
+                        <div className="lg:bg-blue-100 lg:mb-1 bg-gray-200 my-1 rounded-xl py-2 font-bold">Edition: {textbook.edition}</div>
                         
-                        <div className="bg-blue-100 mb-1">ISBN: {textbook.ISBN}</div>
+                        <div className="lg:bg-blue-100 lg:mb-1 bg-gray-200 my-1 rounded-xl py-2 font-bold">ISBN: {textbook.ISBN}</div>
                         
-                        <div className="bg-blue-100 mb-1">Category: {textbook.category.name}</div>
+                        <div className="lg:bg-blue-100 lg:mb-1 bg-gray-200 my-1 rounded-xl py-2 font-bold">Category: {textbook.category.name}</div>
                         
-                        <div className="bg-blue-100">Price: ${textbook.price}</div>
+                        <div className="lg:bg-blue-100 bg-gray-200 my-1 rounded-xl py-2 font-bold">Price: ${textbook.price}</div>
                         
                      </div>
                 
                     
-                    <div className="flex flex-col justify-center mb-44">
+                    <div className="lg:flex lg:flex-col lg:justify-center lg:mb-44 flex justify-evenly">
                         <Link to={`/textbooks/${textbook.id}/edit`}>
-                            <button className="px-48 p-6  bg-blue-600 text-3xl text-white">Edit</button>
+                            <button className=" lg:font-bold lg:px-16 lg:p-6 lg:bg-green-400 lg:hover:bg-green-600 lg:text-3xl lg:text-white  bg-green-400 hover:bg-green-600 my-2 py-2 px-6 text-white font-bold rounded-lg">Edit</button>
                         </Link>
-                        {currentUser ? <button onClick={handleDelete} className=" m-2 mx-6 p-6 bg-red-500 text-3xl text-white ">
+                        {currentUser ? <button onClick={handleDelete} className=" lg:font-bold lg:m-2 lg:mx-36 lg:p-6  lg:bg-red-400 lg:hover:bg-red-600 lg:text-3xl lg:text-white bg-red-400 hover:bg-red-600 my-2 py-2 px-3 text-white font-bold rounded-lg ">
                             Delete
                         </button> : <Link to="/login"> Delete </Link>}
                     </div>
